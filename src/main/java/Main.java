47fb023d3e62f3ad90afa5931a985030fa2f24f0
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-04-02 23:24:59 +0900

    Feat/start screen (#1)
    * feat: startScreen Image draw 확인
    
    * feat: 그림 일부 배치, 버튼 배치
    
    * feat: 그림 전부 배치

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import swing.MainFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
