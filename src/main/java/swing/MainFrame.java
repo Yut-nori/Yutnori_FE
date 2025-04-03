d43b1755d2c3ae8a724f2cf04c89c4597e2057c3
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-04-03 13:35:10 +0900

    Feat/setting screen (#3)
    * fix: gitignore에서 iml파일 git에 올리지 않는 문제 수정
    
    * refactor: File의 이름을 가져오기, image 불러오기 함수 util.File로 이동
    
    * feat: Start->setting 이동 구현, setting의 바탕화면 및 combobox 확인
    
    * feat: setting의 resource 추가

--- src/main/java/swing/MainFrame.java
+++ src/main/java/swing/MainFrame.java
@@ -19,11 +19,13 @@ public class MainFrame extends JFrame {
 
         // 패널 생성
         Start startPanel = new Start(this);
+        Setting settingPanel = new Setting(this);
 
         // 나중에 다른 게임 화면도 여기에 추가 가능
 
         // 패널 등록
         cardPanel.add(startPanel, "start");
+        cardPanel.add(settingPanel, "setting");
 
         add(cardPanel);
         cardLayout.show(cardPanel, "start");
--- src/main/java/swing/Start.java
+++ src/main/java/swing/Start.java
@@ -14,28 +14,27 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
 
+import static swing.util.File.getFileName;
+import static swing.util.File.imageLoading;
+
 public class Start extends JPanel {
-
-    private MainFrame mainFrame;
-    private final Map<String, BufferedImage> images = new HashMap<>();
-
     private final String screenName = "start";
-    private final List<String> imageNames = new ArrayList<>();
+
+    private Map<String, BufferedImage> images = new HashMap<>();
 
     public Start(MainFrame mainFrame) {
-        this.mainFrame = mainFrame;
         setLayout(null);
 
         // 배경 이미지 로딩
-        getFileName();
-        imageLoading();
+        List<String> imageNames = getFileName(screenName);
+        images = imageLoading(imageNames, screenName);
 
         JButton startButton = null;
         JButton exitButton = null;
         if (imageNames.contains("startButton.png")) {
             ImageIcon icon = new ImageIcon(getClass().getResource("/start/startButton.png"));
             startButton = createImageButton(icon, e -> {
-                System.out.println("게임 시작!");
+                mainFrame.showCard("setting");
             });
             startButton.setBounds(820, 280, icon.getIconWidth(), icon.getIconHeight());
         }
@@ -44,6 +43,7 @@ public class Start extends JPanel {
             ImageIcon icon = new ImageIcon(getClass().getResource("/start/exitButton.png"));
             exitButton = createImageButton(icon, e -> {
                 System.out.println("게임 종료!");
+                System.exit(0);  // 콘솔 프로그램 종료
             });
             exitButton.setBounds(850, 460, icon.getIconWidth(), icon.getIconHeight());
         }
@@ -66,39 +66,6 @@ public class Start extends JPanel {
         button.addActionListener(action);
 
         return button;
-    }
-
-    private void getFileName() {
-        URL resource = getClass().getResource("/" + screenName);
-        if(resource != null) {
-            File folder = new File(resource.getPath());  // 이미지가 위치한 폴더
-            File[] files = folder.listFiles((dir, name) -> name.endsWith(".png"));
-            if(files != null) {
-                for (File file : files) {
-                    imageNames.add(file.getName());
-                }
-            } else {
-                System.out.println("File not found");
-            }
-        }
-
-    }
-
-    private void imageLoading() {
-        try {
-            for (String imageName : imageNames) {
-                String imagePath = screenName + "/" + imageName;
-                URL imageUrl = getClass().getClassLoader().getResource(imagePath);
-                if (imageUrl != null) {
-                    BufferedImage img = ImageIO.read(imageUrl);
-                    images.put(imageName, img); // 이미지 이름으로 저장
-                } else {
-                    System.err.println("이미지를 찾을 수 없습니다: " + imagePath);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
