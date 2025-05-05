90a3a7f99ba18eb5272637215f86baf61e2a6f49
seokjin1023 <babyy1023@naver.com>
2025-05-05 23:50:57 +0900

    feat:unitPosition(rectangle) 조정

--- src/main/java/swing/gameBoard/MainBoard.java
+++ src/main/java/swing/gameBoard/MainBoard.java
@@ -1,18 +1,43 @@
 package swing.gameBoard;
 
+import swing.gameBoard.RightPanel.UnitIcon;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static swing.util.File.getFileName;
+import static swing.util.File.imageLoading;
 
 class MainBoard extends JPanel {
-    public MainBoard() {
+    private final String screenName = "GameBoard";
+    private Map<String, BufferedImage> images = new HashMap<>();
+    private final int shape;
+    public MainBoard(int shape) {
         setLayout(null);
-        setBackground(Color.WHITE);
         setBounds(310, 60, 660, 640);
+        this.shape = shape;
+
+        List<String> imageNames = getFileName(screenName);
+        images = imageLoading(imageNames, screenName);
+
+
+    }
+
+    private void addUnit(int x, int y, Color color) {
+        UnitIcon unit = new UnitIcon(color);
+        unit.setBounds(x, y, 30, 30);
+        this.add(unit);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // 여기에 그리기 작업을 추가하세요.
+
+        if(shape == 4)
+            g.drawImage(images.get("rectangle.png"), 0, 0, getWidth(), getHeight(), this);
     }
 }
--- src/main/java/swing/gameBoard/RightPanel/UnitIcon.java
+++ src/main/java/swing/gameBoard/RightPanel/UnitIcon.java
@@ -2,20 +2,33 @@ package swing.gameBoard.RightPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-class UnitIcon extends JComponent {
+public class UnitIcon extends JComponent {
     private final Color color;
     private final int diameter;
     private final int thickness;
 
-    public UnitIcon(Color color, int diameter) {
+    public UnitIcon(Color color) {
         this.color = color;
-        this.diameter = diameter;
+        this.diameter = 28;
         this.thickness = 2;
 
         int fullSize = diameter + thickness;
         setPreferredSize(new Dimension(fullSize, fullSize));
         setOpaque(false);
+
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+        setToolTipText("말 클릭");
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("말 클릭됨!");
+                // 여기에 콜백 또는 게임 로직 호출
+            }
+        });
     }
 
     @Override
