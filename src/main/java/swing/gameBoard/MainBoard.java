6f47ea172c2aa789d2fa67927795cd5a006e5c26
seokjin1023 <babyy1023@naver.com>
2025-05-06 00:26:09 +0900

    feat: pentagon unitPostion 구하기

--- src/main/java/swing/gameBoard/MainBoard.java
+++ src/main/java/swing/gameBoard/MainBoard.java
@@ -18,13 +18,29 @@ class MainBoard extends JPanel {
     private final int shape;
     public MainBoard(int shape) {
         setLayout(null);
-        setBounds(310, 60, 660, 640);
+        setOpaque(false);
+        switch (shape) {
+            case 4:
+                setBounds(310, 60, 660, 640);
+                break;
+            case 5:
+                setBounds(315, 70, 690, 690);
+                break;
+            case 6:
+                setBounds(320, 60, 660, 660);
+                break;
+        }
         this.shape = shape;
 
         List<String> imageNames = getFileName(screenName);
         images = imageLoading(imageNames, screenName);
 
-
+        //ㅎㅎ 선물
+        for(int i = 0; i < shape * 7 + 1; i++) {
+            addUnit(UnitPosition.pentagonUnitPositions[i][0],
+                    UnitPosition.pentagonUnitPositions[i][1],
+                    Color.BLUE);
+        }
     }
 
     private void addUnit(int x, int y, Color color) {
@@ -37,7 +53,19 @@ class MainBoard extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        if(shape == 4)
-            g.drawImage(images.get("rectangle.png"), 0, 0, getWidth(), getHeight(), this);
+        switch(shape) {
+            case 4:
+                g.drawImage(images.get("rectangle.png"), 0, 0, getWidth(), getHeight(), this);
+                break;
+            case 5:
+                g.drawImage(images.get("pentagon.png"), 0, 0, getWidth(), getHeight(), this);
+                break;
+            case 6:
+                g.drawImage(images.get("hexagon.png"), 0, 0, getWidth(), getHeight(), this);
+                break;
+            default:
+                break;
+        }
+
     }
 }
