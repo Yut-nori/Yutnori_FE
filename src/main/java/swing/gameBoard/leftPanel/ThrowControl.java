22499ff79c2d212527578c89d6a96232dd4736e0
1unaram <haram620@icloud.com>
2025-05-08 23:19:21 +0900

    Feat: 주석 추가. 브랜치 이동을 위해 잠시 커밋

--- src/main/java/swing/gameBoard/leftPanel/ThrowControl.java
+++ src/main/java/swing/gameBoard/leftPanel/ThrowControl.java
@@ -17,29 +17,44 @@ class ThrowControl extends JPanel {
         setLayout(null);
         setOpaque(false);
 
+        // [1] case에 따라 윷 던지기 버튼을 만들어야 함
+        // case1: 지정 윷 던지기
         if(gm.getGameState().isTest()) {
+
+            // [1.1] 지정할 윷을 선택하는 콤보박스 생성
             JComboBox<String> yutComboBox = createStyledComboBox(
                     new String[]{"빽도", "도", "개", "걸", "윷", "모"}, 20, 240, 120, 70
             );
-            add(yutComboBox);
+
+            // [1.2] 윷 던지기 버튼 생성 및 이벤트 리스너 추가
             JButton designatedYutThrowBtn = createYutButton("지정 윷", 150, 240, 150);
             designatedYutThrowBtn.addActionListener(e -> {
+
+                // [1.2.1] 콤보박스에서 선택한 윷 결과를 가져옴
                 int yutResult = yutComboBox.getSelectedIndex();
-                if(yutResult == 0) yutResult = -1;
+                if(yutResult == 0) yutResult = -1; // 빽도는 -1로 설정
+
+                // 버튼 클릭이 가능한 phase일 때만 버튼 동작
                 if(gm.getGameState().getCurrentPhase().contains(Phase.BUTTON_CLICK)) {
                     System.out.println("지정 윷 던지기 버튼 + " + yutResult);
+
+                    // 윷 던지기 메서드 호출(인자: 지정한 윷 결과)
                     gm.throwDesignatedYut(yutResult);
                 }
             });
+
+            // [1.3] 패널에 추가
+            add(yutComboBox);
             add(designatedYutThrowBtn);
         }
+        // case2: 랜덤 윷 던지기
         else {
             JButton randomYutThrowBtn = createYutButton("랜덤 윷", 20, 240, 270);
             randomYutThrowBtn.addActionListener(new GlobalButtonListener(gm, "randomThrow"));
             add(randomYutThrowBtn);
         }
 
-        // 윷 결과 패널 생성
+        // [2] 윷 결과 패널 생성
         /**
          * 이제 이 Record를 버튼을 누를 때마다 클릭했을 때 보이는게 달라지도록 만들어져야함!
          * gm을 통해서 만들 수 있을듯!
