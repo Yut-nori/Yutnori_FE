6adaa06f663d7274b2568348aceadf6f1b9cc27e
sjpark1023 <babyy1023@naver.com>
2025-05-09 15:02:54 +0900

    feat: merge frontend and backend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import javax.swing.*;
+
+import controller.BoardManager;
 import swing.MainFrame;
+import view.GameView;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new MainFrame().setVisible(true);
         });
+        GameView gameView = new GameView();
+        gameView.displayWelcome(1,2,2);
     }
 }
