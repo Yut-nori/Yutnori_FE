06c247d9c8b27a1a34c36c74c2e6bf99667a6793
1unaram <haram620@icloud.com>
2025-05-09 19:33:22 +0900

    edit: 주석 및 리팩토링 작업 중

--- src/main/java/swing/ScreenManager.java
+++ src/main/java/swing/ScreenManager.java
@@ -21,6 +21,9 @@ public class ScreenManager {
         start();
     }
 
+
+    // ** 메서드 모음 **
+    // 컨테이너에서 이전 패널을 삭제 -> 새로운 패널을 추가 -> 다시 컨테이너를 그리기
     private void switchPanel(JPanel newPanel) {
         container.removeAll();
         container.add(newPanel);
@@ -28,13 +31,12 @@ public class ScreenManager {
         container.repaint();
     }
 
-
-    // (0)게임 시작 -> (1) 메인 화면
+    // (0)게임 시작 -> (1)메인 화면
     public void start() {
         switchPanel(new Start(this));
     }
 
-    // (1)게임 시작 -> (2)세팅
+    // (1)메인 화면 -> (2)세팅
     public void setting() {
         switchPanel(new Setting(this));
     }
@@ -44,10 +46,11 @@ public class ScreenManager {
         switchPanel(new GameBoard(this));
     }
     
-    // (3)윷놀이 시작 -> (4)게임 종료 
+    // (3)윷놀이 시작 -> (4)게임 종료
     public void end() {
         switchPanel(new End(this));
     }
+
 
     // ** Getters and Setters **
     public GameManager getGm() {
--- src/main/java/swing/screen/End.java
+++ src/main/java/swing/screen/End.java
@@ -1,61 +1,65 @@
 package swing.screen;
 
-import swing.MainFrame;
 import swing.ScreenManager;
 import swing.util.GlobalButtonListener;
-import swing.util.ImageRenderer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static swing.util.Button.createButtonIfExists;
-import static swing.util.Button.createImageButton;
 import static swing.util.File.getFileName;
 import static swing.util.File.imageLoading;
 import static swing.util.ImageRenderer.renderImages;
 
 public class End extends JPanel {
+
+    // ** 멤버 변수 **
     private final String screenName = "end";
+    private Map<String, BufferedImage> images;
 
-    private Map<String, BufferedImage> images = new HashMap<>();
-
+    // ** Constructor **
     public End(ScreenManager sm) {
+
+        // [1] 레이아웃을 null로 설정
         setLayout(null);
 
-        // 배경 이미지 로딩
+        // [2] 배경 이미지 로딩
         List<String> imageNames = getFileName(screenName);
         images = imageLoading(imageNames, screenName);
 
-        JButton startButton = null;
-        JButton exitButton = null;
-        startButton = createButtonIfExists(
+        // [3] 게임 시작, 게임 종료 버튼 생성
+        JButton reStartButton;
+        JButton exitButton;
+        reStartButton = createButtonIfExists(
                 imageNames, "startButton.png", "end", 400, 280,
                 new GlobalButtonListener(sm, "settingPage")
         );
-
         exitButton = createButtonIfExists(
                 imageNames, "exitButton.png", "end", 470, 460,
                 new GlobalButtonListener(sm, "exit")
         );
 
-        if (startButton != null) add(startButton);
+        // [4] 패널에 버튼 추가
+        if (reStartButton != null) add(reStartButton);
         if (exitButton != null) add(exitButton);
 
     }
 
+    // 패널이 그려질 때 호출되는 메서드
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        // [1] 배경 이미지 그리기
         if (images.get("background.png") != null) {
             g.drawImage(images.get("background.png"), 0, 0, getWidth(), getHeight(), this);
         }
 
+        // [2] 이미지 이름, x, y 좌표 정의
         Object[][] imageData = {
                 {"man.png", 50, 100},
                 {"woman.png", 850, 100},
@@ -63,6 +67,7 @@ public class End extends JPanel {
                 {"winner.png", 450, 180}
         };
 
+        // [3] 이미지 그리기
         renderImages(g, this, images, imageData);
 
     }
