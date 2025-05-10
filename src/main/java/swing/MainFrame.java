10f22c3d2d703ead7b85c333cb2df07a50cbb617
1unaram <haram620@icloud.com>
2025-05-10 20:02:14 +0900

    feat: Icon 추가 및 타이틀 변경

--- src/main/java/swing/MainFrame.java
+++ src/main/java/swing/MainFrame.java
@@ -3,11 +3,12 @@ package swing;
 import swing.util.UIConstants;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
 
-        setTitle("YutNori");
+        setTitle("YutNori - Team 13");
         setSize(UIConstants.FRAME_WIDTH, UIConstants.FRAME_HEIGHT + 30); // 30은 제목 표시줄 높이
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // 화면 중앙 정렬
@@ -15,6 +16,11 @@ public class MainFrame extends JFrame {
         GameManager gm = new GameManager(new GameState());
         ScreenManager sm = new ScreenManager(this, gm);
         gm.setScreenManager(sm);
+
+        // Icon 설정
+        Image img = new ImageIcon(getClass().getResource("/start/icon.png")).getImage();
+        setIconImage(img);
+
 
     }
 }
